882d47c40eb16a2ef33958d9876c5df4e6423f72
alaskaw <30860972+alaskaw@users.noreply.github.com>
2021-11-19 22:49:08 +0800

    Update example.py
    add  simple logistic regression

--- example.py
+++ example.py
@@ -2,19 +2,21 @@
 from sklearn.datasets import load_diabetes
 from sklearn.metrics import mean_squared_error
 from linear_regression import *
+from logistic_regression import *
 
+
+
+################################################################
+#linear regression
+################################################################
 x= load_diabetes().data
 y= load_diabetes().target.reshape(-1,1 )
-print(x.shape,y.shape)
- 
+print(x.shape,y.shape) 
 x_train=x[0:300]
 y_train=y[0:300]
 x_test=x[300:]
 y_test=y[300:]
 
-################################################################
-#linear regression
-################################################################
 clf=linear_regresion()
 clf.fit(x_train,y_train,1e-3, 100000) 
 pred=clf.predict(x_train)
@@ -23,4 +25,36 @@ print('train: mse_score=',mse_score)
 pred=clf.predict(x_test)
 mse_score= mean_squared_error(y_test,pred)
 print('test: mse_score=',mse_score)
+
+
+################################################################
+#logistic regression
+################################################################
+            
+from sklearn.datasets import make_classification
+from sklearn.metrics import roc_auc_score
+
+x,y=make_classification(n_samples=10000,n_features=100,n_redundant=0, n_informative=5, n_classes=2,n_clusters_per_class=10,random_state=42)
+y=y.reshape(-1,1)
+print(x.shape,y.shape)
+x_train=x[:7000]
+y_train=y[:7000]
+x_test=x[7000:]
+y_test=y[7000:]
+
+clf=logistic_regression() 
+clf.fit(x_train,y_train,lr=1e-3,epoches=10000)
+pred=clf.predict(x_train)
+auc_score=roc_auc_score(y_train,pred)
+print('train: auc_score=',auc_score)
+pred=clf.predict(x_test)
+auc_score=roc_auc_score(y_test,pred)
+print('test:auc_score=',auc_score)
+
+
+
+
+
+
+
 

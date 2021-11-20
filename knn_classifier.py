f9d12b1070af9202665d5aecde2979487b9b2c1c
alaskaw <30860972+alaskaw@users.noreply.github.com>
2021-11-20 23:35:39 +0800

    Update knn_classifier.py

--- knn_classifier.py
+++ knn_classifier.py
@@ -4,8 +4,10 @@ from collections import Counter
 
 
 class knn_classifier:
-    def __init__(self,x,y):
-        self.x_train=x 
+    def __init__(self):
+        pass
+    def train(self,x,y):
+        self.x_train=x
         self.y_train=y
         
     def calc_dist(self,x_test):
@@ -15,15 +17,34 @@ class knn_classifier:
         sim=np.sqrt((-2*sim+ tr.reshape(1,-1) + te.reshape(-1,1)))
         return sim
     
-    def predict_label(self,sim,k=1):
+    def predict_label(self,x_test,sim,k=1):
         num_test=x_test.shape[0 ] 
         y_pred=np.zeros(num_test)
         for i in range(num_test):
             y_pred[i]=Counter(self.y_train[np.argsort(sim[i,:])].flatten()[0:k]).most_common(1)[0][0]
         return y_pred 
+    
+    def cross_validation(self,x,y,k_list=[1,3,5,7,10,20,30,50,80,100]):
+        n_fold=5
+        x=np.array_split(x,n_fold)
+        y=np.array_split(y,n_fold) 
+        
+        for k in k_list:
+            for ifold in range(n_fold):
+                x_tr=np.concatenate(x[:ifold]+x[ifold+1:])
+                y_tr=np.concatenate(y[:ifold]+y[ifold+1:])
+                x_te=x[ifold]
+                y_te=y[ifold]
+                self.train(x_tr,y_tr)
+                sim=self.calc_dist(x_te)
+                pred=self.predict_label(x_te,sim,k=k)
+                acc= accuracy_score(y_te,pred)
+                print(f'k={k},acc={acc}') 
 
-      
-      
+                
 
-      
- 
+                
+                
+                
+                
+                
